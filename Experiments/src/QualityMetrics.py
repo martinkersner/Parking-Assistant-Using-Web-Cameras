72eb21e8eeb4ba5e5e6432b26ee4464ce45743c4
Martin Kersner <m.kersner@gmail.com>
2015-01-23 15:44:47 +0100

    Percentage of Bad Matching Pixels

--- Experiments/src/QualityMetrics.py
+++ Experiments/src/QualityMetrics.py
@@ -5,9 +5,10 @@
 #
 # Quality Metrics for purpose of comparing computed disparity map and ground
 # truth disparity map.
+# Expects greyscale images with range 0 to 255.
 #
-# Root Mean Square Error
-# Percentage of Bad Matching Pixels
+# RMSE  Root Mean Square Error
+# BM    Percentage of Bad Matching Pixels
 #
 # m.kersner@gmail.com
 # 01/23/2015
@@ -15,6 +16,10 @@
 import numpy as np
 
 class QualityMetrics:
+    '''
+    Disparity map and ground truth disparity map are interchangeable due to
+    power of 2 in RMSE and absolute value in BM.
+    '''
 
     dc = None  # disparity map
     dt = None  # groudn truth disparity map
@@ -36,3 +41,13 @@ class QualityMetrics:
         '''
         noe = self.dc.size # number of elements
         return np.sqrt(np.sum(np.power(self.dc-self.dt, 2).reshape((1, noe))) / noe)
+
+    def bm(self, threshold=1.0):
+        '''
+        Percentage of Bad Matching Pixels
+        '''
+        noe = 1.0*self.dc.size # number of elements
+        mask = np.fabs(self.dc-self.dt) > threshold
+
+        return np.sum(mask.reshape((1, noe))) / noe
+
