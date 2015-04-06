66847fe3412a8d73a76beb9f5d54e34d90079c04
Martin Kersner <m.kersner@gmail.com>
2015-04-06 19:45:08 +0200

    added property file for parking assistant

---
+++ ParkingAssistant/Properties.h
@@ -0,0 +1,17 @@
+#ifndef PROPERTIES_H_
+#define PROPERTIES_H_
+
+#define IMG_WIDTH       640
+#define IMG_HEIGHT      480
+
+#define CHESS_ROWS      9
+#define CHESS_COLS      6 
+#define CALIB_TIMESPAN  100
+#define CALIB_IMAGES    21
+#define CALIB_DIR       "Chessboard/"
+
+#define CAM_PROP_INT    "intrinsics.yml"
+#define CAM_PROP_EXT    "extrinsics.yml"
+#define CAM_PROP_DIS    "distortion_camera_models.yml"
+
+#endif // PROPERTIES_H_
